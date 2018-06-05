3ff68caa6796064151dbffc6f69c9db24d308c06
William Park <wjypark94@gmail.com>
2018-06-05 01:32:25 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="header-portfolio">
+        <header>
+          <h1 className="header-h1">
+            William Park Portfolio
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
