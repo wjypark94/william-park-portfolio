88ea674271ecada3d0d342d058cd7935a89611f0
William Park <wjypark94@gmail.com>
2018-06-06 01:36:19 -0700

    updated about component with more styling

--- src/components/About.js
+++ src/components/About.js
@@ -7,19 +7,17 @@ class About extends Component {
         return(
             <div id="about" className="about">
                 <div className="about-content">
-                <h2>About</h2>
-                <p>Hey, I'm William Park, a full-stack software engineer based in Los Angeles, CA.</p>
+             
+                    <h2>About</h2>
+                    <p>Hey, I'm William Park, a full-stack software engineer based in Los Angeles, CA.</p>
 
-                <p> Skills </p>
-                <p>React, Redux, Node, Expres, Mongo, Mongoose, JavaScript, Jquery, HTML, CSS</p>
+                    <p className="ListHeader"> Skills </p>
+                    <p>React, Redux, Node, Express, Mongo, Mongoose, JavaScript, Jquery, HTML, CSS</p>
 
 
                 </div>
                 <div className="about-img-content">
                     <img className="profile-img" src={Profile} alt="profile"></img>
-                </div>
-                <div className="about-bio-content">
-                    <p>This is the about section for William Park</p>
                 </div>
             </div>
         )
