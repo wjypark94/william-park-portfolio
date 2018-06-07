b0c99aa8e118accaa6eca80393cd6b88f3ad8f10
William Park <wjypark94@gmail.com>
2018-06-07 00:01:36 -0700

    removed mui theme provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-    <MuiThemeProvider>
-        <App />
-    </MuiThemeProvider>,
+<App />, 
 document.getElementById('root')
 );
 registerServiceWorker();
