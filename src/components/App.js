764d2003ad139b00cdb339b0396267b39cf25178
William Park <wjypark94@gmail.com>
2018-06-11 18:35:54 -0700

    added fontawesome icons and links to contact

--- src/components/App.js
+++ src/components/App.js
@@ -16,11 +16,25 @@ class App extends Component {
           <div className="header">
               <h2 className="site-title">Will Park</h2>
               <h1 className="site-description">Full Stack Web Developer</h1>
+              <div className="header-links">
+                <a href="mailto:wjypark94@gmail.com">
+                  <i className="icon fa fa-envelope"></i>
+                </a>
+                <a href="https://github.com/wjypark94">
+                 <i className="icon fa fa-github-alt" aria-hidden="true"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/wjypark">
+                  <i className="icon fa fa-linkedin" aria-hidden="true"></i>
+                </a>
+              </div>
+          </div>
+          <div className="arrow-wrapper">
+            <i className="fa fa-chevron-down smooth-container" aria-hidden="true"></i>
           </div>
           </section>
         <section className="main-body">
-          <Work/>
           <About/>
+          <Work/>
           <Contact/>
 
         </section>
